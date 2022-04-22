60f2ef20bf22f749061c9633b1f1fb05ad2747a7
tutorial6 <manapure.vaibhav542@gmail.com>
2022-04-22 18:48:36 +0530

    Calculate Salary of EmployeeWage

--- EmployeeCheck.js
+++ EmployeeCheck.js
@@ -1,6 +1,6 @@
 // UC1-Check employee Present or absent
 
-const IS_ABSENT=0;
+/*const IS_ABSENT=0;
 console.log(Math.random()*10);
 let empCheck=Math.floor(Math.random()*10)%2;
 
@@ -11,3 +11,29 @@ if(empCheck==IS_ABSENT)
 else{
 console.log("Employee is present");
 }
+*/
+
+//UC2
+
+const IS_Part_Time=1;
+const IS_Full_Time=2;
+const PART_TIME_HOURS=4;
+const FULL_TIME_HOURS=8;
+const WAGE_PER_HOURS=20;
+
+let empHrs=0;
+let empCheck=Math.floor(Math.random()*10)%3;
+
+switch(empCheck)
+{
+    case IS_Part_Time;
+    empHrs=PART_TIME_HOURS;
+    break;
+    case IS_Full_Time;
+    empHrs=FULL_TIME_HOURS;
+    break;
+    default:
+        empHrs=0;
+}
+let empWage=empHrs*WAGE_PER_HOURS;
+console.log("EmpWage: "+empWage);
