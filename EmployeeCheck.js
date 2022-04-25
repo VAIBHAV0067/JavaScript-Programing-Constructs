8c72fb8ce68badc4a45720068f3a33f9908c31c1
tutorial6 <manapure.vaibhav542@gmail.com>
2022-04-25 19:00:05 +0530

    Calculating Wages till Number of Working Days or Total Working hours per Month is Reached

--- EmployeeCheck.js
+++ EmployeeCheck.js
@@ -40,7 +40,7 @@ console.log("EmpWage: "+empWage);*/
 
 //UC3
 
-const IS_Part_Time=1;
+/*const IS_Part_Time=1;
 const IS_Full_Time=2;
 const PART_TIME_HOURS=4;
 const FULL_TIME_HOURS=8;
@@ -65,4 +65,77 @@ switch(empCheck)
 }
 let empWage=empHrs*WAGE_PER_HOURS;
 
-console.log("EmpWage: "+empWage);
+console.log("EmpWage: "+empWage);*/
+ 
+//UC4
+
+/*const IS_PART_TIME=1;
+const IS_FULL_TIME=2;
+const PART_TIME_HOURS=4;
+const FULL_TIME_HOURS=8;
+const WAGE_PER_HOUR=20;
+
+function getWorkingHours(empCheck)
+{
+switch(empCheck)
+{
+    case IS_PART_TIME:
+        return PART_TIME_HOURS;
+
+    case IS_FULL_TIME:
+          return FULL_TIME_HOURS;
+    default:
+        return 0;
+
+}
+}
+const NUM_OF_WORKING_DAYS=2;
+
+let empHrs=0;
+
+for(let day=0;day<NUM_OF_WORKING_DAYS;day++)
+{
+    let empCheck=Math.floor(Math.random()*10)%3;
+    empHrs+=getWorkingHours(empCheck);
+}
+
+let empWage=empHrs*WAGE_PER_HOUR;
+console.log("Total Hrs:" +empHrs+"EmpWage: "+empWage);*/
+
+//UC5-while loop
+
+const IS_PART_TIME=1;
+const IS_FULL_TIME=2;
+const PART_TIME_HOURS=4;
+const FULL_TIME_HOURS=8;
+const WAGE_PER_HOUR=20;
+
+function getWorkingHours(empCheck)
+{
+switch(empCheck)
+{
+    case IS_PART_TIME:
+        return PART_TIME_HOURS;
+
+    case IS_FULL_TIME:
+          return FULL_TIME_HOURS;
+    default:
+        return 0;
+
+}
+}
+
+const MAX_HRS_IN_MONTH = 160;
+const NUM_OF_WORKING_DAYS =20;
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+while (totalEmpHrs <= MAX_HRS_IN_MONTH &&
+    totalWorkingDays < NUM_OF_WORKING_DAYS)
+    {
+        totalWorkingDays++;
+        let empCheck = Math.floor(Math.random() *10) % 3;
+        totalEmpHrs += getWorkingHours(empCheck);
+    }
+    let empWage = totalEmpHrs * WAGE_PER_HOUR;
+    console.log("UC5 - Total Days: " + totalWorkingDays +
+    "Total Hrs: " +totalEmpHrs +" Emp Wage: " +empWage);
